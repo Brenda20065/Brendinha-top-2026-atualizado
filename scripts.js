0913709f8623e4777bfedc1f5dab3aed9b81d67d
Brenda20065 <brendasilva.f2004@gmail.com>
2026-05-28 10:02:32 -0400

    corrige a transição do efeito hover no menu

--- scripts.js
+++ scripts.js
@@ -2,11 +2,12 @@ const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const items = document.querySelectorAll('.item');
 const dots = document.querySelectorAll('.dot');
-const numbersIndicator = document.querySelector('.numbers'); 
+const numbersIndicator = document.querySelector('.numbers');
 const list = document.querySelector('.list');
 
 let active = 0;
 const total = items.length;
+let timer;
 
 function update(direction) {
     const currentActiveItem = document.querySelector('.item.active');
@@ -20,7 +21,7 @@ function update(direction) {
         if (active === total) {
             active = 0;
         }
-    } 
+    }
     else if (direction < 0) {
         active = active - 1;
         if (active < 0) {
@@ -35,6 +36,11 @@ function update(direction) {
         numbersIndicator.textContent = String(active + 1).padStart(2, '0');
     }
 }
+
+clearInterval(timer);
+timer = setInterval(() => {
+    update(1);
+}, 5000);
 
 prevButton.addEventListener('click', () => update(-1));
 nextButton.addEventListener('click', () => update(1));
